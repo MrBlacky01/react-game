91ee1ba7b2e62dd967e6f92632253748cc324449
Dmitry M <mr.blacky01@gmail.com>
2021-03-03 17:17:11 +0100

    fix: change server settings

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const app = express()
 const path = require('path')
 const port = process.env.PORT || 3001
 
-app.use('/', express.static(path.join(__dirname, 'public')))
-
-app.listen(port, () => console.log("Listening on Port", port)) 
+//app.use('/', express.static(path.join(__dirname, 'dist')))
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('dist'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+})
+};
+app.listen(port, () => console.log("Listening on Port", port));
